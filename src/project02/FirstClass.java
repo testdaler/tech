95302ddc60a5c92b97b351533ec34ee3b028311d
testdaler <72760585+testdaler@users.noreply.github.com>
2020-11-08 17:54:58 +0100

    second commit from remote master branch

--- src/project02/FirstClass.java
+++ src/project02/FirstClass.java
@@ -9,6 +9,7 @@ public class FirstClass {
 		
 		System.out.println("third comid from local master");
 		System.out.println("first commit from remoot master branch");
+		System.out.println("second commit from remote master branch");
 		
 		
 
