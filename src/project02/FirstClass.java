4d04f7be37d15864a8f560861b950347f9e35d74
testdaler <testdaler@gmail.com>
2020-11-08 18:28:43 +0100

    ohiron islo

--- src/project02/FirstClass.java
+++ src/project02/FirstClass.java
@@ -4,19 +4,16 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("daler");
-		
 		System.out.println("second commit from local master");
-		
 		System.out.println("third comid from local master");
 		System.out.println("first commit from remoot master branch");
 		System.out.println("second commit from remote master branch");
 		System.out.println("Third commit from remoot master branch");
 		System.out.println("Here is conflict  from remoot master branch");
 		System.out.println("second conflict  from remoot master branch");
-		
 		System.out.println("4 commit");
 		System.out.println("Here is a merg conflict I create");
-	System.out.println("conflictmeshava hozi");	
+	    System.out.println("conflictmeshava hozi");	
 
 	}
 
